ef0faa680d40fa8f24160d953a9e6d4e968c19d3
Emmanuel Oyewole <oyewoleemmanuel300@gmail.com>
2024-05-14 14:27:37 +0100

    updated tail config with custom css properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    width:{
+      'cusw': '27rem',
+      'cardw': '28rem',
+      'profilew':'2rem'
+    },
+    height:{
+      'cush': '35rem',
+      'cardh': '15rem',
+      'profileh': '2rem'
+    },
+    borderRadius:{
+      myrad: '1.2rem',
+      bro: '0.3rem'
+    },
+    colors:{
+      myel: 'hsl(47, 88%, 63%)',
+      mywh: 'hsl(0, 0%, 100%)',
+      mygry: 'hsl(0, 0%, 50%)'
+    },
+    fontFamily:{
+      mfont:['Figtree']
+    },
+    fontWeight:{
+      myweit: '500',
+      myweit2: '800'
+    },
     extend: {},
   },
   plugins: [],
